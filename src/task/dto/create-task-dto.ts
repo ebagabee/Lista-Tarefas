3a0c15213b21092ea1337cd52c16be0a8d1783d7
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-08-04 19:59:05 -0300

    Creating Task Service

--- src/task/dto/create-task-dto.ts
+++ src/task/dto/create-task-dto.ts
@@ -1,17 +1,14 @@
 import { IsNotEmpty, IsString, IsOptional, IsDate, IsISO8601 } from 'class-validator';
 
 export class CreateTaskDto {
-    // titulo
     @IsNotEmpty()
     @IsString()
     title: string;
 
-    // descricao
     @IsOptional()
     @IsString()
     description: string;
     
-    // deadline
     @IsOptional()
     @IsISO8601()
     deadline: string;
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, Inject } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task-dto';
+import { UpdateTaskDto } from './dto/update-task-dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TaskEntity } from './entities/task.entity'
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+    constructor(
+        @InjectRepository(TaskEntity)
+        private readonly taskRepository: Repository<TaskEntity>,
+    ) { }
+
+    async create(
+        createTaskDto: CreateTaskDto,
+    ): Promise<TaskEntity> {
+        const taskData = this.taskRepository.create(createTaskDto);
+        return await this.taskRepository.save(taskData);
+    }
+
+    async findAll(): Promise<TaskEntity[]> {
+        return await this.taskRepository.find();
+    }
+
+    async findOne(id: number): Promise<TaskEntity> {
+        const taskData = await this.taskRepository.findOneBy({ id });
+
+        if (!taskData) {
+            throw new HttpException('Usuário não encontrado', 404);
+        }
+
+        return taskData;
+    }
+
+    async update(id: number, UpdateTaskDto: UpdateTaskDto,): Promise<TaskEntity> {
+        const existingTask = await this.findOne(id);
+        const taskData = this.taskRepository.merge(existingTask, UpdateTaskDto);
+
+        return await this.taskRepository.save(taskData);
+    }
+
+    async remove(id: number): Promise<TaskEntity> {
+        const existingTask = await this.findOne(id);
+        return await this.taskRepository.remove(existingTask);
+    }
+}
