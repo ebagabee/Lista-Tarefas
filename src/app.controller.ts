27b6ea629383f356443799d236954e0811ec69af
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-08-07 17:06:04 -0300

    Removendo arquivos nao utilizados

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getStarted(): string {
+    return this.appService.getStarted();
   }
 }
